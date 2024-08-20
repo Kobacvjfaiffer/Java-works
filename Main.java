230b9c323978afb9496a52bf140082ff83e07ffb
Kobacvjfaiffer <162374510+Kobacvjfaiffer@users.noreply.github.com>
2024-08-20 11:28:06 -0400

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,28 +1,23 @@
-import javax.swing.*;
-
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
-        /* operadores condicioneles
-        == igual
-        != diferente
-        > mayor
-        >= mayorigual
-        < menor
-        <= menorigual
-
-         */
+   public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
-        int numero, dato = 5;
-        numero = Integer.parseInt(JOptionPane.showInputDialog("Digite un numero: "));
+     /*   System.out.printf("Hello and welcome!");
 
-        if (numero == dato){
-            JOptionPane.showMessageDialog(null,"El numero es 5 ");
+        for (int i = 1; i <= 5; i++) {
+            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
+            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
+            System.out.println("i = " + i);
         }
-        else {
-            JOptionPane.showMessageDialog(null,"El numero es diferente de 5 ");
-        }
-        }
+        //String apellidos = "faiffer Ames";
+        //System.out.println(apellidos);
+*/
+        String myArray[];
+        myArray = new String[3];
+        myArray[0] = "Kobac";
+        myArray[1] = "Victor";
+        myArray[2] = "Javier";
+      }
     }
